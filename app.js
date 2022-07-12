4cea0064030a4e8d871328d4b4ac6876bf0aa978
Kevin Aviles <kevin.aviles@arbusta.net>
2022-07-12 11:47:59 -0300

    creacion de vistas y app

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 const express = require('express');
 const app = express();
+const port = 3000;
+app.set('view engine','ejs');
+app.set('views',__dirname+'/views');
+app.use(express.static(__dirname+'/public'));
 
+app.use((req,res,next)=>{
+    res.status(404).render("404",{
+        titulo:"Página 404",
+        laUrl:req.url
+    })
+})
+
+app.listen(port,()=>{
+    console.log(`
+    ***********************
+    Servidor corriendo en el puerto:${port}
+    
+    link: ----> http://localhost:${port}
+
+    ***********************
+    `);
+})
